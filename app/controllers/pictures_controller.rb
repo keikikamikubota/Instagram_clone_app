6139c3cf1dc5fd8ca813a20195e35713d621cd53
keiki <kkeki0410@gmail.com>
2023-08-16 18:01:45 +0900

    controller pictures作成

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,16 @@
+class PicturesController < ApplicationController
+  def new
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def confirm
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'pictures/new'
+  get 'pictures/index'
+  get 'pictures/edit'
+  get 'pictures/show'
+  get 'pictures/confirm'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
