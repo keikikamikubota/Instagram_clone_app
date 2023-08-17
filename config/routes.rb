a1ec114aa8532efa6b78d598b8c258c3eee0dde6
keiki <kkeki0410@gmail.com>
2023-08-17 10:57:48 +0900

    sessionsコントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'pictures#index'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
